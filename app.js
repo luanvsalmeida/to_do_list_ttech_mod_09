ff16e2a71a27d339912de29414afc802a3f70e23
luanvsalmeida <luanalmeida@alunos.utfpr.edu.br>
2024-11-29 00:43:21 -0300

    Create index.mustache
    - Create HTML template for TODO app with Bootstrap.
    - Includes task list, task creation form, and login form.
    - Update app.js, style.css

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var tasksRouter = require('./routes/tasks');
 
 var app = express();
 
-app.use(logger('dev'));
+// view engine setup
+var mustacheExpress = require("mustache-express");
+var engine = mustacheExpress();
+app.engine("mustache", engine);
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'mustache');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', tasksRouter);
+var indexRouter = require('./routes/index');
+var apiRouter = require('./routes/tasks');
+
+app.use('/', indexRouter);
+app.use('/api/tasks', apiRouter);
 
 module.exports = app;
